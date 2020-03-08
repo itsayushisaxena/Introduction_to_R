e735aff08bae75d1f7db3f886c745ed57e66ef9a
Ayushi Saxena <45483037+AyushiSaxena11@users.noreply.github.com>
2020-03-08 13:04:17 +0530

    Add files via upload

---
+++ R_basics.R
@@ -0,0 +1,32 @@
+#used for comment
+#R is case-sensitive
+#Arithmetic Operations
+3+5 #addition
+3-1 #subtraction
+5*6 #multiplication
+9/4 #division
+50 %% 5 #modulo gives remainder
+
+#Variable_Assignment
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_apples+my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges #variables are useful for reusability
+
+#Basic data types in R(Numerics(3.4),Integer(4),Boolean(TRUE,FALSE),Character("ROCK"))
+# Assign my_numeric to be 42
+my_numeric <- 42
+
+# Assign my_character to be "universe"
+my_character <- "universe"
+
+# Assign my_logical to be FALSE
+my_logical <- FALSE
+
+# Check class or datatype of variable,my_numeric
+class(my_numeric)
